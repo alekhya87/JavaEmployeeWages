6965b05fe4aab4603474a32745c7e05a0cc4d69d
alekhya87 <davulurialekhya87909.com>
2020-10-17 08:30:31 +0530

    EmployeeAttendence

--- src/programs/EmployeeAttendence.java
+++ src/programs/EmployeeAttendence.java
@@ -1,10 +1,15 @@
 package programs;
-
 import java.util.Scanner;
 import utility.Utilities;
-
+/*
+ * @author:alekhya
+ * @created date:17th oct 2020
+ * @functionality:attendence of employee if they are
+ *                  present or absent
+ */
 public class EmployeeAttendence {
     public static void main(String[] argv){
+        System.out.println("Welcome To Employee Wage Computation");
         Scanner scanner=new Scanner(System.in);
         Utilities utilities=new Utilities();
         double result=utilities.Attendence();
--- src/utility/Utilities.java
+++ src/utility/Utilities.java
@@ -1,5 +1,16 @@
 package utility;
+/*
+ * @author:alekhya
+ * @created date:17th oct 2020
+ * @functionality:this is a utility file in which it contain the
+ *                logic of code for source file
+ */
 public class Utilities {
+    /*
+    * functionality:this method contain the logic of adding two numbers
+    * @return     empCheck        - the output values
+    */
+
     int IsFullTime=1;
     public double Attendence(){
         double empCheck =Math.floor(Math.random()*10)%2;
